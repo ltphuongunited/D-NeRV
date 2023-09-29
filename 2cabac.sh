c94d6f790c154c3cab7b5a351b69cf99af2f7695
DanbiAubrey <dobbydarnbi@gmail.com>
2023-09-29 11:39:17 +1000

    update bppnew

--- 2cabac.sh
+++ 2cabac.sh
@@ -1,35 +1,27 @@
 CUDA_VISIBLE_DEVICES=1  python train.py --model_type HDNeRV2 --model_size L \
-        --eval_only --weight checkpoints/HDNeRV2/L.pth -b 4 -p 500 --method cabac --qp -34
+        --eval_only --weight checkpoints/HDNeRV2/L.pth -b 4 -p 500 --method cabac --qp -22
 
 CUDA_VISIBLE_DEVICES=1  python train.py --model_type HDNeRV2 --model_size L \
-        --eval_only --weight checkpoints/HDNeRV2/L.pth -b 4 -p 500 --method cabac --qp -38
+        --eval_only --weight checkpoints/HDNeRV2/L.pth -b 4 -p 500 --method cabac --qp -26
 
 CUDA_VISIBLE_DEVICES=1  python train.py --model_type HDNeRV2 --model_size L \
-        --eval_only --weight checkpoints/HDNeRV2/L.pth -b 4 -p 500 --method cabac --qp -42
+        --eval_only --weight checkpoints/HDNeRV2/L.pth -b 4 -p 500 --method cabac --qp -30
 
 CUDA_VISIBLE_DEVICES=1 python train.py --model_type HDNeRV2 --model_size M \
-        --eval_only --weight checkpoints/HDNeRV2/M.pth -b 4 -p 500 --method cabac --qp -34
+        --eval_only --weight checkpoints/HDNeRV2/M.pth -b 4 -p 500 --method cabac --qp -22
 
 CUDA_VISIBLE_DEVICES=1 python train.py --model_type HDNeRV2 --model_size M \
-        --eval_only --weight checkpoints/HDNeRV2/M.pth -b 4 -p 500 --method cabac --qp -38
+        --eval_only --weight checkpoints/HDNeRV2/M.pth -b 4 -p 500 --method cabac --qp -26
         
 CUDA_VISIBLE_DEVICES=1 python train.py --model_type HDNeRV2 --model_size M \
-        --eval_only --weight checkpoints/HDNeRV2/M.pth -b 4 -p 500 --method cabac --qp -42
+        --eval_only --weight checkpoints/HDNeRV2/M.pth -b 4 -p 500 --method cabac --qp -30
 
 CUDA_VISIBLE_DEVICES=1 python train.py --model_type HDNeRV2 --model_size S \
-        --eval_only --weight checkpoints/HDNeRV2/S.pth -b 4 -p 500 --method cabac --qp -34
+        --eval_only --weight checkpoints/HDNeRV2/S.pth -b 4 -p 500 --method cabac --qp -22
 
 CUDA_VISIBLE_DEVICES=1 python train.py --model_type HDNeRV2 --model_size S \
-        --eval_only --weight checkpoints/HDNeRV2/S.pth -b 4 -p 500 --method cabac --qp -38
+        --eval_only --weight checkpoints/HDNeRV2/S.pth -b 4 -p 500 --method cabac --qp -26
 
 CUDA_VISIBLE_DEVICES=1 python train.py --model_type HDNeRV2 --model_size S \
-        --eval_only --weight checkpoints/HDNeRV2/S.pth -b 4 -p 500 --method cabac --qp -42
+        --eval_only --weight checkpoints/HDNeRV2/S.pth -b 4 -p 500 --method cabac --qp -30
 
-
-
-hdnerv2:
-  compression_dir: compression/hdnerv2
-  embedding_path: "ompressed_embedding.ncc"
-  raw_decoder_path: "raw_decoder.pt"
-  stream_path: "compressed_decoder.nnc"
-  compressed_decoder_path: "compressed_decoder_converted.pt"
